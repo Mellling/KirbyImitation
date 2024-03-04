26b8ca29f79fcff462a04e325b03a7f57f65730d
Mellling <154119653+Mellling@users.noreply.github.com>
2024-03-05 00:50:49 +0900

    feat. 별의 커비 애니메이션(idle, walk, jump), 커비 걷기/점프 구현

--- Assets/Scripts/Kriby.cs
+++ Assets/Scripts/Kriby.cs
@@ -8,19 +8,109 @@ public class Kriby : MonoBehaviour
     [Header("Componemt")]
     [SerializeField] Rigidbody2D rigid;
     [SerializeField] SpriteRenderer render;
+    [SerializeField] Animator animator;
+
+    [Header("Property")]
     [SerializeField] float movePower;
+    [SerializeField] float brakePower;
+    [SerializeField] float maxXSpeed;
+    [SerializeField] float jumpSpeed;
+
+    [SerializeField] LayerMask groundCheakLayer;
+
     private Vector2 moveDir;
+    private bool isGround;
 
     private void Update()
     {
         Move();
+        CheakJumpSituation();
+
+        animator.SetBool("IsGround", isGround);
     }
     public void Move()
     {
-        rigid.AddForce(Vector2.right * moveDir * movePower);
+        if (moveDir.x < 0 && rigid.velocity.x > -maxXSpeed)
+        {
+            rigid.AddForce(Vector2.right * moveDir * movePower);
+        }
+        else if (moveDir.x > 0 && rigid.velocity.x < maxXSpeed)
+        {
+            rigid.AddForce(Vector2.right * moveDir * movePower);
+        }
+        else if (moveDir.x == 0 && rigid.velocity.x < 0)
+        {
+            rigid.AddForce(Vector2.right * brakePower);
+        }
+        else if (moveDir.x == 0 && rigid.velocity.x > 0)
+        {
+            rigid.AddForce(Vector2.left * brakePower);
+        }
     }
     private void OnMove(InputValue value)
     {
         moveDir = value.Get<Vector2>();
+        
+        if (moveDir.x < 0)
+        {
+            render.flipX = true;
+            animator.SetBool("Walking", true);
+        }
+        else if (moveDir.x > 0)
+        {
+            render.flipX = false;
+            animator.SetBool("Walking", true);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+    }
+
+    public void Jump()
+    {
+        Vector2 velocity = rigid.velocity;
+        velocity.y = jumpSpeed;
+        rigid.velocity = velocity;
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed && isGround)
+        {
+            Jump();
+            animator.SetBool("Jumping", true);
+        }
+    }
+
+    private void CheakJumpSituation()
+    {
+        if (rigid.velocity.y > 0)
+        {
+            animator.SetBool("Jumping", true);
+
+        }
+        else if (rigid.velocity.y <= 0)
+        {
+            animator.SetBool("Jumping", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("In");
+        if (groundCheakLayer.Contain(collision.gameObject.layer))
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Out");
+        if (groundCheakLayer.Contain(collision.gameObject.layer))
+        {
+            isGround = false;
+        }
     }
 }
